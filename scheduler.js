c127347dc137f62dfc8af7bbdbd12d45517ddf28
Eric Kryski <e.kryski@gmail.com>
2013-11-28 19:42:32 -0700

    adding redis to go connection string support

--- scheduler.js
+++ scheduler.js
@@ -1,9 +1,21 @@
-var kue = require('kue');
-var jobs = kue.createQueue();
 var _ = require('lodash');
 var EventEmitter = require('events').EventEmitter;
+var url = require('url');
+var kue = require('kue');
+var redis = require('redis');
+
+if (process.env.REDISTOGO_URL) {
+  var rtg = require("url").parse(process.env.REDISTOGO_URL);
+
+  kue.redis.createClient = function() {
+    var client = redis.createClient(rtg.port, rtg.hostname);
+    client.auth(rtg.auth.split(":")[1]);
+    return client;
+  };
+}
 
 var _emitter = new EventEmitter();
+var jobs = kue.createQueue();
 
 var Scheduler = {
   on: function( name, callback, scope ) {
@@ -12,12 +24,13 @@ var Scheduler = {
 
   run: function() {
     console.log('Scheduler Running');
+
+    this.processJob();
       
-    setInterval(_.bind(this.processJob, this), 1000);
+    // setInterval(_.bind(this.processJob, this), 1000);
   },
 
   processJob: function(){
-    console.log('Looking for Video Job');
     jobs.process('video', 1, function(job, done){
       // Emit event for worker to listen to
       _emitter.emit('process:video', job, done);
--- server.js
+++ server.js
@@ -1,8 +1,21 @@
+var url = require('url');
 var kue = require('kue');
-var jobs = kue.createQueue();
-// var scheduler = require('./scheduler');
+var redis = require('redis');
 
 var port = process.env.port || 3000;
+
+if (process.env.REDISTOGO_URL) {
+  var rtg = require("url").parse(process.env.REDISTOGO_URL);
+
+  kue.redis.createClient = function() {
+    var client = redis.createClient(rtg.port, rtg.hostname);
+    client.auth(rtg.auth.split(":")[1]);
+    return client;
+  };
+}
+// var scheduler = require('./scheduler');
+
+
 
 // scheduler.run();
 
